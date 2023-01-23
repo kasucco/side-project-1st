436f43131c81f25f85592ed766e29b9fa19d03b0
sujingkim <suqpus99@gmail.com>
2023-01-23 23:38:39 +0900

    add: useinputs 아직 로그인 에러 많음

--- src/Hooks/useInputs.tsx
+++ src/Hooks/useInputs.tsx
@@ -1,14 +1,21 @@
-// import { useState, useCallback } from 'react';
+import { useState, useCallback, ChangeEvent } from "react";
+// type onChangeType = (e :ChangeEvent<HTMLInputElement>)=>void;
 
-// function useInputs(initialForm) {
-//   const [form, setForm] = useState(initialForm);
-//   // change
-//   const onChange = useCallback(e => {
-//     const { name, value } = e.target;
-//     setForm(form => ({ ...form, [name]: value }));
-//   }, []);
-//   const reset = useCallback(() => setForm(initialForm), [initialForm]);
-//   return [form, onChange, reset];
-// }
+interface userState {
+  userId?: string;
+  password?: string;
+}
+function useInputs(initialForm: userState | null) {
+  const [form, setForm] = useState(initialForm);
+  // change
+  const onChangeHandler = useCallback<
+    (e: ChangeEvent<HTMLInputElement>) => void
+  >((e) => {
+    const { name, value } = e.target;
+    setForm((form) => ({ ...form, [name]: value }));
+  }, []);
+  //   const reset = useCallback(() => setForm(initialForm), [initialForm]);
+  return [form, setForm, onChangeHandler];
+}
 
-// export default useInputs;
+export default useInputs;
